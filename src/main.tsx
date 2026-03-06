a9288c071aa5b96f355c8550d05b98774407bd1c
pgonia8-wq <pgonia8@gmail.com>
2026-03-06 17:16:13 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
 import './index.css'
 import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { ThemeProvider } from './lib/ThemeContext'
-import App from './App.tsx'
 import ChatPage from './pages/chat/ChatPage.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <MiniKitProvider appId="app_6a98c88249208506dcd4e04b529111fc">
-      <ThemeProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />} />
-            <Route path="/chat" element={<ChatPage />} />
-            <Route path="*" element={<App />} />
-          </Routes>
-        </BrowserRouter>
-      </ThemeProvider>
-    </MiniKitProvider>
-  </React.StrictMode>
+  <MiniKitProvider appId="app_6a98c88249208506dcd4e04b529111fc">
+    <ThemeProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/chat" element={<ChatPage />} />
+          <Route path="*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </MiniKitProvider>
 )
