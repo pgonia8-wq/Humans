b671d01dceee7a4303e6815360ccf6551abc0ec9
pgonia8-wq <pgonia8@gmail.com>
2026-03-06 15:30:49 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.tsx'  // ← ahora se usa App.tsx
 import './index.css'
 import { MiniKitProvider } from '@worldcoin/minikit-js/minikit-provider'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-
 import { ThemeProvider } from './lib/ThemeContext'
-import HomePage from './pages/HomePage.tsx'
-import ChatPage from './pages/chat/ChatPage.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <MiniKitProvider appId="app_6a98c88249208506dcd4e04b529111fc">
-    <ThemeProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/chat" element={<ChatPage />} />
-          <Route path="*" element={<HomePage />} />
-        </Routes>
-      </BrowserRouter>
-    </ThemeProvider>
-  </MiniKitProvider>
+  <React.StrictMode>
+    <MiniKitProvider appId="app_6a98c88249208506dcd4e04b529111fc">
+      <ThemeProvider>
+        <App />  // ← App.tsx maneja routes y verify
+      </ThemeProvider>
+    </MiniKitProvider>
+  </React.StrictMode>
 )
