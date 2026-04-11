7535d4dd7134e24731af49a64c07a3e813ab0572
Hbyhumans <pgonia8@gmail.com>
2026-04-11 15:39:22 -0600

    refactor: use custom MiniKitProvider (Love pattern), remove package import

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,14 @@ import React from "react";
   import App from "./App.tsx";
   import "./index.css";
 
-  import { MiniKitProvider } from "@worldcoin/minikit-js/minikit-provider";
+  import MiniKitProvider from "./components/minikit-provider";
   import { ThemeProvider } from "./lib/ThemeContext";
   import { UserProvider } from "./context/UserContext";
   import { LanguageProvider } from "./LanguageContext";
   import ErrorBoundary from "./components/ErrorBoundary";
 
-  const APP_ID = (import.meta as any).env?.VITE_APP_ID ?? "";
-
   ReactDOM.createRoot(document.getElementById("root")!).render(
-    <MiniKitProvider appId={APP_ID}>
+    <MiniKitProvider>
       <UserProvider>
         <ThemeProvider>
           <LanguageProvider>
