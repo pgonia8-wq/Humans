02148f7d883cf5d4e82b1c92a5a5fa0b083b6002
Hbyhumans <pgonia8@gmail.com>
2026-04-14 15:15:50 -0600

    Update TotemOracle.sol

--- tótem/contracts/TotemOracle.sol
+++ tótem/contracts/TotemOracle.sol
@@ -5,62 +5,75 @@ contract TotemOracle {
 
     address public signer;
 
-    constructor(address _signer) {
-        signer = _signer;
-    }
-
     struct Data {
         uint256 score;
         uint256 influence;
-        uint256 nonce;
         uint256 timestamp;
+        uint256 nonce;
     }
 
     mapping(address => Data) public data;
     mapping(address => uint256) public nonces;
+    mapping(address => uint256) public lastUpdate;
 
-    event Updated(address indexed totem, uint256 score, uint256 influence);
+    uint256 public constant MIN_INTERVAL = 30;
+
+    constructor(address _signer) {
+        signer = _signer;
+    }
 
     function update(
         address totem,
         uint256 score,
         uint256 influence,
         uint256 nonce,
-        bytes memory sig
+        bytes calldata sig
     ) external {
 
-        require(nonce == nonces[totem], "BAD_NONCE");
+        require(score <= 10000, "Bad score");
+        require(influence >= 80 && influence <= 120, "Bad influence");
+
+        require(nonce == nonces[totem], "Bad nonce");
+
+        require(
+            block.timestamp >= lastUpdate[totem] + MIN_INTERVAL,
+            "Too fast"
+        );
 
         bytes32 hash = keccak256(
             abi.encodePacked(totem, score, influence, nonce)
         );
 
+        require(recover(hash, sig) == signer, "Bad sig");
+
+        data[totem] = Data(score, influence, block.timestamp, nonce);
+
+        nonces[totem]++;
+
+        lastUpdate[totem] = block.timestamp;
+    }
+
+    function recover(bytes32 hash, bytes memory sig) internal pure returns (address) {
         bytes32 ethHash = keccak256(
             abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)
         );
 
-        require(recover(ethHash, sig) == signer, "BAD_SIG");
+        (bytes32 r, bytes32 s, uint8 v) = split(sig);
 
-        data[totem] = Data(score, influence, nonce, block.timestamp);
-
-        nonces[totem]++;
-
-        emit Updated(totem, score, influence);
+        return ecrecover(ethHash, v, r, s);
     }
 
-    function recover(bytes32 hash, bytes memory sig)
-        internal pure returns (address)
-    {
-        bytes32 r;
-        bytes32 s;
-        uint8 v;
+    function split(bytes memory sig) internal pure returns (bytes32 r, bytes32 s, uint8 v) {
+        require(sig.length == 65, "Bad sig");
 
         assembly {
             r := mload(add(sig, 32))
             s := mload(add(sig, 64))
             v := byte(0, mload(add(sig, 96)))
         }
+    }
 
-        return ecrecover(hash, v, r, s);
+    function getInfluence(address user) external view returns (uint256) {
+        return data[user].influence;
     }
 }
