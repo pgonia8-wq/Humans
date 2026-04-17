a34deea8fdb900008b8c4260a638f207cab2419a
Hbyhumans <pgonia8@gmail.com>
2026-04-17 11:08:16 -0600

    Create TotemAntiManipulationLayer.sol

---
+++ tótem/contracts/TotemAntiManipulationLayer.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/access/Ownable2Step.sol";
+
+contract TotemAntiManipulationLayer is Ownable2Step {
+
+    mapping(address => uint256) public lastOracleUpdate;
+    mapping(address => uint256) public emaPrice;
+
+    uint256 public alpha = 20; // smoothing factor
+
+    event OracleUpdated(address indexed user, uint256 value);
+
+    function updateOracle(address user, uint256 newValue) external onlyOwner {
+
+        require(block.timestamp > lastOracleUpdate[user] + 15 minutes, "too fast");
+
+        uint256 prev = emaPrice[user];
+
+        if (prev == 0) {
+            emaPrice[user] = newValue;
+        } else {
+            emaPrice[user] = (prev * (100 - alpha) + newValue * alpha) / 100;
+        }
+
+        lastOracleUpdate[user] = block.timestamp;
+
+        emit OracleUpdated(user, emaPrice[user]);
+    }
+
+    function getSafeValue(address user) external view returns (uint256) {
+        return emaPrice[user];
+    }
+}
