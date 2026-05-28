765db2442b39e2af60bde27e4bd5660a487fb176
Hbyhumans <pgonia8@gmail.com>
2026-05-28 10:58:22 -0600

    fix: src/main.tsx — iOS scroll, crash & Supabase fixes

--- src/main.tsx
+++ src/main.tsx
@@ -1,25 +1,24 @@
 import React from "react";
-  import ReactDOM from "react-dom/client";
-  import App from "./App.tsx";
-  import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-  import MiniKitProvider from "./components/minikit-provider";
-  import { ThemeProvider } from "./lib/ThemeContext";
-  import { UserProvider } from "./context/UserContext";
-  import { LanguageProvider } from "./LanguageContext";
-  import ErrorBoundary from "./components/ErrorBoundary";
+import MiniKitProvider from "./components/minikit-provider";
+import { ThemeProvider } from "./lib/ThemeContext";
+import { UserProvider } from "./context/UserContext";
+import { LanguageProvider } from "./LanguageContext";
+import ErrorBoundary from "./components/ErrorBoundary";
 
-  ReactDOM.createRoot(document.getElementById("root")!).render(
-    <MiniKitProvider>
-      <UserProvider>
-        <ThemeProvider>
-          <LanguageProvider>
-            <ErrorBoundary>
-              <App />
-            </ErrorBoundary>
-          </LanguageProvider>
-        </ThemeProvider>
-      </UserProvider>
-    </MiniKitProvider>
-  );
-  
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <MiniKitProvider>
+    <UserProvider>
+      <ThemeProvider>
+        <LanguageProvider>
+          <ErrorBoundary>
+            <App />
+          </ErrorBoundary>
+        </LanguageProvider>
+      </ThemeProvider>
+    </UserProvider>
+  </MiniKitProvider>
+);
